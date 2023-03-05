f5d1e4b3854a2f7af066c86e63fb18dd2db987e3
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-03-05 14:00:29 +0100

    create account JS

---
+++ JS/create-account.js
@@ -0,0 +1,36 @@
+const API_BASE_URL = "https://nf-api.onrender.com";
+
+async function registerUser(url, data) {
+  try {
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, postData);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    window.location.href = "sign-in.html";
+  }
+}
+
+const createUser = document.querySelector("#create-user-form");
+
+createUser.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userData = {
+    name: createUser.username.value,
+    email: createUser.email.value,
+    password: createUser.password.value,
+  };
+
+  registerUser(`${API_BASE_URL}/api/v1/auction/auth/register`, userData);
+});
