991bc1e4314b316294daf08e2ad3e8c5be77ceda
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-03-05 14:47:34 +0100

    sign in JS

---
+++ JS/sign-in.js
@@ -0,0 +1,41 @@
+const API_BASE_URL = "https://nf-api.onrender.com";
+
+async function registerUser(url, data) {
+  try {
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, postData);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    const accessToken = json.accessToken;
+    localStorage.setItem(`accessToken`, accessToken);
+    return json;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    // window.location.href = "feed.html";
+  }
+}
+
+const signInUser = document.querySelector("#sign-in-user");
+
+signInUser.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userData = {
+    email: signInUser.email.value,
+    password: signInUser.password.value,
+  };
+
+  registerUser(`${API_BASE_URL}/api/v1/auction/auth/login`, userData);
+});
+
+// willand_test
+// willand-test@noroff.no
+// willandpass123
