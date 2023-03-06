033ff4caaf03aeb54dc8baf7c72a692e7d4f177e
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-03-06 11:50:42 +0100

    post work

--- JS/feed.js
+++ JS/feed.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = "https://nf-api.onrender.com";
+const feed = document.querySelector(".feed-container");
 
 async function getWithToken(url) {
   try {
@@ -13,10 +14,25 @@ async function getWithToken(url) {
 
     const response = await fetch(url, fetchOptions);
     const json = await response.json();
-    console.log(json);
+
+    feed.innerHTML = "";
+
+    for (let i = 0; i < json.length; i++) {
+      console.log(json[i]);
+
+      feed.innerHTML += `
+      <div class="post">
+        <div class="user-info">
+            <img src="${json[i].author.avatar}">
+            <h2>${json[i].author.name}</h2>
+        </div>
+        <p>${json[i].title}</p>
+        <p>${json[i].body}</p>
+      </div>`;
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
-getWithToken(`${API_BASE_URL}/api/v1/social/posts`);
+getWithToken(`${API_BASE_URL}/api/v1/social/posts/?_author=true`);
