394eb2dc4e9511b61ea192e3c98b1493f21408ec
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-03-11 12:13:55 +0100

    adding mixins and making a placeholder for avatars

--- JS/feed.js
+++ JS/feed.js
@@ -4,6 +4,7 @@ const feed = document.querySelector(".feed-container");
 async function getWithToken(url) {
   try {
     const token = localStorage.getItem("accessToken");
+    const userName = localStorage.getItem("name");
     const fetchOptions = {
       method: "GET",
       headers: {
@@ -52,20 +53,45 @@ async function getWithToken(url) {
     });
 
     json.forEach((item) => {
-      feed.innerHTML += `      <div class="post">
-        <div class="user-info">
-            <img src="${item.author.avatar}">
-            <h2>${item.author.name}</h2>
-        </div>
-         <a href="single-entry.html?id=${item.id}">
-             <p>${item.title}</p>
-            <p>${item.body}</p>
-         </a>
-      </div>`;
+      feed.innerHTML += `
+    <div class="post">
+      <div class="user-info">
+        
+        ${
+          item.author.avatar
+            ? '<img src="${item.author.avatar}">'
+            : '<div class="no-avatar"><i class="fa-solid fa-user"></i></div>'
+        }
+        <h2>${item.author.name}</h2>
+      </div>
+      <a href="single-entry.html?id=${item.id}">
+        <p>${item.title}</p>
+        <p>${item.body}</p>
+      </a>
+      ${item.author.name === userName ? "<button>Delete</button>" : ""}
+    </div>`;
     });
+
+    function myPosts() {
+      let userPosts = [];
+      for (let i = 0; i < json.length; i++) {
+        if (json[i].author.name === userName) {
+          userPosts.push(json[i]);
+        }
+      }
+      return userPosts;
+    }
+
+    console.log(myPosts());
   } catch (error) {
     console.log(error);
   }
+
+  const postsSelect = document.querySelector("#select-posts");
+  const selectPosts = (e) => {
+    if ((postsSelect.value = "my-posts")) {
+    }
+  };
 }
 
 getWithToken(`${API_BASE_URL}/api/v1/social/posts/?_author=true`);
